636cf71a09f0907529dbcf2595eb6ee67992fb15
Alexey <eleksius@mail.ru>
2026-05-25 19:55:33 +0300

    some fixes

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ from datetime import date
 app.app_context().push()
 db.create_all()
 
-# admin = User(
-#     username="eleksius",
-#     password_hash=generate_password_hash("10Qpalzm$"),
-#     name="Алексей",
-#     is_admin=True,
-#     birthday=date(2009, 6, 9),
-#     emoji='😎'
-# )
-user = User(
-    username="stesha",
-    password_hash=generate_password_hash("qwerty"),
-    name="Степанида",
-    is_admin=False,
-    birthday=date(1488, 6, 7),
-    emoji='😺'
+admin = User(
+    username="eleksius",
+    password_hash=generate_password_hash("10Qpalzm$"),
+    name="Алексей",
+    is_admin=True,
+    birthday=date(2009, 6, 9),
+    emoji='😎'
 )
-# db.session.add(admin)
-db.session.add(user)
+# user = User(
+#     username="stesha",
+#     password_hash=generate_password_hash("qwerty"),
+#     name="Степанида",
+#     is_admin=False,
+#     birthday=date(1488, 6, 7),
+#     emoji='😺'
+# )
+db.session.add(admin)
+# db.session.add(user)
 db.session.commit()
 exit()
